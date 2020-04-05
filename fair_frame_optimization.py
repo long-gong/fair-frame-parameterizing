b5438f553a16d0e9302d38eadfb97a604e9b1e08
Long Gong <long.github@gmail.com>
2020-04-05 16:04:27 -0400

    fixed a corner case

--- fair_frame_optimization.py
+++ fair_frame_optimization.py
@@ -19,6 +19,23 @@ def constraint(n, rho, delta):
     T = batch_size(n, rho, delta)
     return delta * (1.0 / rho + n + n * rho * T)
 
+def solve_constraint(n, rho):
+    lower = 0.0
+    upper = 1.0 / (1.0 / rho + n + n * rho)
+    cnt = 1
+    while np.abs(upper -lower) > 1e-10:
+        mid = (lower + upper) / 2.0
+        if constraint(n, rho, mid) < 1.0:
+            lower = mid 
+        else:
+            upper = mid 
+        cnt += 1
+        if cnt > 1e3:
+            break
+    if constraint(n, rho, mid) < 1.0:
+        return mid
+    else:
+        return None 
 
 CONTEXT_SETTINGS = dict(help_option_names=['-h', '--help'])
 
@@ -36,7 +53,14 @@ def parameterize(port_number, load_bound):
     delta_min = result.x[0]
     c = constraint(port_number, load_bound, delta_min)
     print(f'delta_min: {delta_min}')
-    print(f'constraint: {c} < 1')
+    if c < 1.0:
+        print(f'constraint: {c} < 1')
+    else:
+        print('constraint not satisfied')
+        delta_min = solve_constraint(port_number, load_bound)
+        print(f'new delta_min: {delta_min}')
+        c = constraint(port_number, load_bound, delta_min)
+        print(f'constraint: {c} < 1')
     T = batch_size(port_number, load_bound, delta_min)
     print(f'batch size: {T}')
 
